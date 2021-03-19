22d1f3e2e56bf53cb072fad7a3266a76a258a229
PanhcharothPenh <80951385+PanhcharothPenh@users.noreply.github.com>
2021-03-19 16:39:41 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<br>
+<div class="container-fluid footer">
+    <div class="row">
+    <br>
+        <div class="col-md-10 col-md-push-2">
+        <div style="text-align: left;" class="col-md-3">
+		<img src="images/invoice.png" width="487px" class="img-rounded left" aligh>
+        </div>
+        <div style="text-align: left;" class="col-md-2">
+        </div>
+        <div style="text-align: left;" class="col-md-2"><h5 style="font-size: 17px;" >Contact US</h5>
+        <p><a style="color: #767773" href="#">Building #66, Street 63 Corner St 154, Sangkat Phsar Thmei I, Khan Doun Penh, Phnom Penh, Cambodia.</a><br>
+		<a style="color: #767773" href="#">(+855) 17 986 356</a></p></div>
+			<h3 class="widget-title">PAYMENTS METHOD</h3>
+		<img width="310" height="40" src="https://ptc-computer.com.kh/wp-content/uploads/2020/08/Payway.png" class="image wp-image-10999  attachment-full size-full" alt="" loading="lazy" style="max-width: 100%; height: auto;" title="PAYMENTS METHOD" srcset="https://ptc-computer.com.kh/wp-content/uploads/2020/08/Payway.png 310w, https://ptc-computer.com.kh/wp-content/uploads/2020/08/Payway-300x40.png 300w" sizes="(max-width: 310px) 100vw, 310px">
+        </div>
+        </div>
+    </div>
+   <div class="container-fluid foot2">
+    <div class="row">
+        <div style="padding: 10px;" class="col-md-10 col-md-push-2">
+Copyright © 2021 Panhcharoth. All rights reserved.
+        </div>
+    </div>
+    </div>
